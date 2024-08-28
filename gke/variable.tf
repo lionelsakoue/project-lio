822d436b9e771853fa5c582dc928f3e6d17b6b9c
lionelsakoue <130628424+lionelsakoue@users.noreply.github.com>
2024-08-27 21:07:53 -0400

    Update variable.tf

--- gke/variable.tf
+++ gke/variable.tf
@@ -1,29 +1,29 @@
 variable "cluster_name" {
   description = "The name of the Kubernetes cluster."
   type        = string
-  default     = "my-gke-cluster"  # Default value for cluster name
+  default     = "my-gke-cluster"
 }
 
 variable "region" {
   description = "The GCP region in which the cluster will be created."
   type        = string
-  default     = "us-central1"     # Default value for region
+  default     = "us-central1"
 }
 
 variable "initial_node_count" {
   description = "The number of nodes to initially create in the cluster."
   type        = number
-  default     = 1                 # Default value for initial node count
+  default     = 1
 }
 
 variable "machine_type" {
   description = "The machine type to use for nodes in the cluster."
   type        = string
-  default     = "e2-micro"   # Default value for machine type
+  default     = "e2-micro"
 }
 
 variable "disk_size_gb" {
   description = "The size of the disk in gigabytes."
   type        = number
-  default     = 10
+  default     = 20
 }
