9892dd8c6ef9efa2472e17e33e3509fa14c29d92
lionelsakoue <sakouepierre@gmail.com>
2024-10-19 19:06:04 -0400

    added values

--- math.sh
+++ math.sh
@@ -7,9 +7,11 @@ echo "Enter first number:"
 read num1
 echo "Enter second number:"
 read num2
+echo "enter third number"
+read "num3"
 
 # Perform addition
-result=$((num1 + num2))
+result=$((num1 + num2 + num3))
 
 # Output the result
 echo "The result of addition is: $result"
